e58f1d148a7840807ac6e14f1a1c2473e5c9561e
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-10-31 15:48:02 +0530

    Update pallete.java

--- pallete.java
+++ pallete.java
@@ -1,10 +1,6 @@
-/*
-  TEAM 1
-  LAB EX 13
- */
-package lab.ex.pkg13;
 
-//importing packages
+package labex13;
+
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -12,34 +8,32 @@ import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import javax.swing.*;
 
-//main class
-//using AdjustmentListener for the ScrollBar to work
+
 class pallete implements AdjustmentListener  
 { 
-    JFrame f;//FRAME
+    JFrame f;
     JScrollBar s,s1,s2;
     
-pallete()//Constructor
+pallete()
 {  
 f= new JFrame("Scrollbar Example");
-//Initial values are set to 128--->as mentioned in the question
-s=new JScrollBar(JScrollBar.VERTICAL, 128, 225, 0, 255);  //scroll bar 1
-s1=new JScrollBar(JScrollBar.VERTICAL, 128, 0, 0, 255); //scroll bar 2
-s2=new JScrollBar(JScrollBar.VERTICAL, 128, 225, 0, 255); //scroll bar 3
+s=new JScrollBar(JScrollBar.VERTICAL, 128, 225, 0, 255); 
+s1=new JScrollBar(JScrollBar.VERTICAL, 128, 0, 0, 255);
+s2=new JScrollBar(JScrollBar.VERTICAL, 128, 225, 0, 255); 
 s.setBounds(100,100, 30,100);  
 s1.setBounds(140,100, 30,100);
 s2.setBounds(180,100, 30,100);
 s.addAdjustmentListener(this);
 s1.addAdjustmentListener(this);
 s2.addAdjustmentListener(this);
-//adding ScrollBars to Frame
+
 f.add(s);  
 f.add(s1); 
 f.add(s2); 
 f.setSize(500,500);  
 f.setLayout(null);  
 f.setVisible(true); 
-f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Closing the JFrame
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }  
 public void adjustmentValueChanged(AdjustmentEvent ae)
 {
